8c75c453c6a86082a271c7526dd9bf5c8eeecf24
Pramod Saji <pramod.saji94@gmail.com>
2023-02-09 18:20:04 -0500

    Modification to make alignment changes

--- src/App.js
+++ src/App.js
@@ -30,45 +30,47 @@ function App() {
   return (
     <div className="App">
       <h1>ToDo App</h1>
-      <form>
-        <TextField
-          id="standard-basic"
-          label="Fire away!!!"
-          variant="standard"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <br />
-        <br />
-        <Button variant="contained" onClick={addTodo}>
-          Add
-        </Button>
-      </form>
-      <div className="todo-list">
-        {todos.length !== 0 ? (
-          todos.map((todo, index) => {
-            return (
-              <ListItem
-                className="list-item"
-                key={index}
-                secondaryAction={
-                  <IconButton
-                    edge="end"
-                    aria-label="delete"
-                    onClick={() => deleteTodo(index)}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                }
-              >
-                <ListItemText primary={todo} />{" "}
-              </ListItem>
-            );
-          })
-        ) : (
-          <div className="empty-message">Nothing to do...Enjoy!!!</div>
-        )}
-      </div>
+      <center>
+        <form>
+          <TextField
+            id="standard-basic"
+            label="Fire away!!!"
+            variant="standard"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <br />
+          <br />
+          <Button variant="contained" onClick={addTodo}>
+            Add
+          </Button>
+        </form>
+        <div className="todo-list">
+          {todos.length !== 0 ? (
+            todos.map((todo, index) => {
+              return (
+                <ListItem
+                  className="list-item"
+                  key={index}
+                  secondaryAction={
+                    <IconButton
+                      edge="end"
+                      aria-label="delete"
+                      onClick={() => deleteTodo(index)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemText primary={todo} />{" "}
+                </ListItem>
+              );
+            })
+          ) : (
+            <div className="empty-message">Nothing to do...Enjoy!!!</div>
+          )}
+        </div>
+      </center>
     </div>
   );
 }
