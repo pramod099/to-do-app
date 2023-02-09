32968284d369bd66314b4c4ea6917448bd5f20e8
Pramod Saji <pramod.saji94@gmail.com>
2023-02-09 16:28:19 -0500

    Assignment02-ToDoApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (input !== "") {
+      setTodos([...todos, input]);
+    }
+    console.log(todos);
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(
+      todos.filter((todo, i) => {
+        return index !== i;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo App</h1>
+      <form>
+        <TextField
+          id="standard-basic"
+          label="Fire away!!!"
+          variant="standard"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <br />
+        <br />
+        <Button variant="contained" onClick={addTodo}>
+          Add
+        </Button>
+      </form>
+      <div className="todo-list">
+        {todos.length !== 0 ? (
+          todos.map((todo, index) => {
+            return (
+              <ListItem
+                className="list-item"
+                key={index}
+                secondaryAction={
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => deleteTodo(index)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                }
+              >
+                <ListItemText primary={todo} />{" "}
+              </ListItem>
+            );
+          })
+        ) : (
+          <div className="empty-message">"Nothing to do...Enjoy!!!" </div>
+        )}
+      </div>
     </div>
   );
 }
